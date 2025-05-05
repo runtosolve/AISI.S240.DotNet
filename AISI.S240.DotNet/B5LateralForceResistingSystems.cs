60db6e32979012b8206658626fd3847df3f938fa
Cris Moen <cris.moen@runtosolve.com>
2025-05-05 10:41:48 -0400

    work on shear wall strength table

---
+++ AISI.S240.DotNet/B5LateralForceResistingSystems.cs
@@ -0,0 +1,23 @@
+﻿namespace AISI.S240.DotNet;
+
+public class B5LateralForceResistingSystems
+{
+    public static Dictionary<string, double> TableB5_2_2_3__1()
+    {
+
+        Dictionary<string, Dictionary<string, double>> table = new Dictionary<string, Dictionary<string, double>>();
+        
+        
+        Dictionary<string, double> wallShearStrength = new Dictionary<string, double>();
+
+        wallShearStrength.Add("6/12", 485.0);
+        wallShearStrength.Add("4/12", Double.NaN);
+        wallShearStrength.Add("3/12", Double.NaN);
+        wallShearStrength.Add("2/12", Double.NaN);
+
+        table.Add("0.18 in. steel sheet", wallShearStrength);
+        
+        return wallShearStrength;
+    }
+
+}
--- AISI.S240.DotNet/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace AISI.S240.DotNet;
-
-public class Class1
-{
-}
